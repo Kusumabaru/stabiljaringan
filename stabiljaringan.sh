0bbbf539c9ad941931ec3b227f89ce850664f5aa
Kusumabaru <86560735+Kusumabaru@users.noreply.github.com>
2022-04-12 19:36:47 +0700

    Add files via upload

---
+++ stabiljaringan.sh
@@ -0,0 +1,30 @@
+clear
+
+sleep 1
+echo "selamat datang di sc by ©kusumabaru"
+echo "follow githubme:https://github.com/kusumabaru"
+echo "==============================================="
+echo
+sleep 2
+echo
+echo "silahkan pilih tols nya bang"
+echo
+echo "1) stabilkan jaringan"
+echo "============================"
+echo "2) keluar"
+echo "============================"
+read -p "pilih mana =" bro
+
+if [ $bro = 1 ]  || [ $bro = 1 ]
+then
+clear
+ping -s1000 1.1.1.1
+fi
+
+if [ $bro = 2 ]  || [ $bro = 2 ]
+then
+clear
+echo "selamat tinggal"
+sleep 3
+exit
+fi
